5b631b2705c6514c1710ba67df390faeee872d0f
Toby Mackenzie <public@tobymackenzie.com>
2022-10-14 12:48:13 -0400

    tests: fix name of test class

--- tests/DumpTest.php
+++ tests/DumpTest.php
@@ -5,7 +5,7 @@ use TJM\Dev;
 use TJM\Dev\DumpValue;
 use PHPUnit\Framework\TestCase;
 
-class Test extends TestCase{
+class DumpTest extends TestCase{
 	// public function setUp(){ //-! uncomment for PHP < 7.0
 	public function setUp(): void{
 		require_once(__DIR__ . '/inc/A.php');
