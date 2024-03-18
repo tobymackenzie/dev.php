573139be94aaa30df09af83da2feec0077d67d17
Toby Mackenzie <public@tobymackenzie.com>
2024-03-18 16:48:32 -0400

    code: fix deprecations for assigning undefined object properties

--- src/DumpValue.php
+++ src/DumpValue.php
@@ -2,6 +2,10 @@
 namespace TJM\Dev;
 
 class DumpValue{
+	public $content;
+	public $file;
+	public $name;
+	public $type;
 	public function __construct($arg){
 		foreach($arg as $key=> $value){
 			$this->$key = $value;
