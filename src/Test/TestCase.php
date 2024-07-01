c2f08335df9703bfe7a4609238110b7138b27484
Toby Mackenzie <public@tobymackenzie.com>
2024-07-01 16:05:40 -0400

    feat(tests): add `assertException` assertion to `TestCase`

--- src/Test/TestCase.php
+++ src/Test/TestCase.php
@@ -1,9 +1,19 @@
 <?php
 namespace TJM\Dev\Test;
+use Exception;
 use ReflectionMethod;
 use PHPUnit\Framework\TestCase as Base;
 
 abstract class TestCase extends Base{
+	protected function assertException($expect, $run, $message = null){
+		try{
+			$run();
+		}catch(Exception $e){
+			$this->assertInstanceOf($expect, $e, "Exception should be instance of {$expect}");
+			return true;
+		}
+		$this->fail($message ?: "No exception thrown, {$expect} expected");
+	}
 	public function doReflectionMethodTest(string $objectClass, string $method, array $expects, array $objectArgs = []){
 		$object = new $objectClass(...$objectArgs);
 		$rmethod = new ReflectionMethod($objectClass, $method);
--- tests/TestCaseTest.php
+++ tests/TestCaseTest.php
@@ -1,11 +1,17 @@
 <?php
 namespace TJM\Dev\Tests;
+use Exception;
 use TJM\Dev\Test\Expect;
 use TJM\Dev\Test\ExpectArgs;
 use TJM\Dev\Test\TestCase;
 use TJM\Dev\Tests\Src\Reflect;
 
 class TestCaseTest extends TestCase{
+	public function testAssertException(){
+		$this->assertException(Exception::class, function(){
+			throw new Exception('fail');
+		});
+	}
 	public function testDoReflectionMethodTest(){
 		return $this->doReflectionMethodTest(Reflect::class, 'getValue', [
 			'a'=> 'a++',
